babdbeb74b6337445d7a8cb4b7ed6c72c21a1dd7
Thomas Miller <thomasmiller@Thomass-MBP.gateway>
2017-12-27 22:55:49 +1000

    testing infura-integration

---
+++ README.md
@@ -0,0 +1,5 @@
+# infura-integration
+
+// I created these curl commands using this tutorial: https://blog.infura.io/getting-started-with-infura-28e41844cc89
+
+// Next steps I'll use the Web3 Javascript API
---
+++ infura-integration.js
@@ -0,0 +1,7 @@
+curl -X POST --data '{"jsonrpc": "2.0", "method": "web3_clientVersion", "prams":[],"id":1}' https://mainnet.infura.io/INFURATOKEN
+
+
+
+
+curl -X POST --data '{"jsonrpc": "2.0", "method": "eth_blockNumber", "prams":[],"id":1}' https://mainnet.infura.io/INFURATOKEN
+
